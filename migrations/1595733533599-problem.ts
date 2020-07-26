e42b70c500634bd72655027bf2313fb92e86b282
KH <moltak@gmail.com>
2020-07-26 14:39:04 +0900

    fixed minor issues

--- migrations/1595733533599-problem.ts
+++ migrations/1595733533599-problem.ts
@@ -3,7 +3,7 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class problem1595733533599 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      'CREATE TABLE "problem" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "problem_text" varchar NOT NULL, "type" integer NOT NULL, "choices" varchar default "", "answer" varchar NOT NULL)',
+      'CREATE TABLE "problem" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "problem_text" varchar default "", "type" integer NOT NULL, "choices" varchar default "", "answer" varchar NOT NULL)',
     );
   }
 
